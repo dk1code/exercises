eb6b98df118af252201801cb45619779cc1fbd58
dk1code <dk1code@web.de>
2020-05-13 11:45:18 +0200

    finished exercise; added .gitignore

---
+++ PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,39 @@
+var x = 2, y = 4;
+
+console.log("1:", x + y);
+console.log("2:", x - y);
+console.log("3:", x * y);
+console.log("4:", x / y);
+
+// 5.
+var z = 10;
+
+let resultOne = x * y / z;
+console.log(resultOne);
+
+// 5.2
+let a = 15;
+let b = 9;
+console.log("5.2:", a / b);
+
+// 6.
+let c = 20;
+let resultTwo = a + b * c;
+console.log("6:", resultTwo);
+
+// 7.
+a++;
+console.log("7:", a);
+
+// 8.
+
+b--;
+console.log("8:", b);
+
+// 9.
+
+let d = b - a;
+console.log("9:", d+c);
+
+// 10.
+console.log("10:", resultOne % resultTwo);

5f6caaa2d1074da8a60d4e4bd2c0655ac22e7498
dk1code <dk1code@web.de>
2020-05-14 11:08:57 +0200

    Daniel: solved exercise 03

---
+++ 03-PB-console/daniel.js
@@ -0,0 +1,27 @@
+// 1.
+
+console.log("this ia an exercise in console logging");
+
+// 2.
+
+let variable = "This is the value.";
+console.log(variable);
+
+// 3.
+
+let firstName = "Daniel";
+let lastName = "Kalz";
+console.log(firstName, lastName);
+
+// 4.
+
+let name = "Daniel";
+let city = "Berlin";
+let district = "Lichtenberg";
+let country = "Germany";
+let job = "Student";
+console.log(name, city, district, country, job);
+
+// 5.
+
+console.log("This is " + name + ". He lives in " + city + "-" + district + ". He ist from " + country + ". His current occupation is: " + job + ".");

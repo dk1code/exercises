d3bfc63b247ab18a0f8818a26d57175fdb0b83d0
dk1code <dk1code@web.de>
2020-05-18 09:41:23 +0200

    finished exercise 06

---
+++ 06-PB-ternary-exercises/daniel.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// 1.
+
+let isDog;
+console.log(isDog ? "1. pat,pat" : "1.", "find me a dog to pat!");
+  
+// 2.
+
+let speedCheck;
+console.log(speedCheck >= 50 ? "2. You're going to fast!" : "2. You're driving below the speed limit, Oma!");
+
+// 3.
+
+let age;
+console.log(age < 16 ? "3. serve butter beer" : "3.", "serve beer");
+
+// 4.
+
+let isStudent;
+console.log(isStudent ? "4. Ticket costs €5,00" : "4.", "Ticket costs €12,00");
+
+// 5.
+
+let okMarie;
+console.log(okMarie === "cake" ? "5. Let them eat cake" : "5.", "Oh, bother");

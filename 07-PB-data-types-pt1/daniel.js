2dde1ff7fadd0e1006cbd0405a1175b5dfc1e561
dk1code <dk1code@web.de>
2020-05-18 14:32:33 +0200

    Daniel: finished 07

---
+++ 07-PB-data-types-pt1/daniel.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// 1.
+
+let stringOne = "This is a string!";
+console.log("1.", stringOne);
+
+// 2.
+
+console.log("2.", stringOne[1], stringOne[5]);
+
+// 3.
+
+let name = "Max";
+
+// 4.
+
+let age = 20;
+
+// 5.
+
+let year = 2020;
+
+// 6.
+
+console.log("6.", `${name} is ${age} in ${year}`);
+
+// 7.
+
+let isMarried = false;
+
+// 8.
+
+console.log(`${name} ist married: ${isMarried}`);
+
+// 9.
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+
+console.log("9.", `${programmingLanguage} is fun: ${isFun}`);
+
+// 10.
+
+console.log("10.", programmingLanguage[0], programmingLanguage[4]);
+
+// 11.
+
+console.log("11.", programmingLanguage.length);

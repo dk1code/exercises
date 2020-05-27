4d1c8327279fd0767dd83afafbec9eb89e9a481e
dk1code <dk1code@web.de>
2020-05-27 14:33:42 +0200

    finished 17

---
+++ 17-PB-program-blockscope/17-daniel.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// 1.
+
+let determiner = -5;
+
+{
+  let x = null;
+  if (determiner < 0) {
+    x = "Less then 0";
+  } else {
+    x = "Greater or equal to 0";
+  }
+  console.log("1:", x);
+}
+
+// 2.
+
+{
+  let updater = null;
+  if (determiner >= 0) {
+    updater = "Greater or equal to 0";
+    let message = "Positive Integer";
+    console.log(`2: Determiner is "${updater}", with a value of a ${message}`);
+  } else {
+    updater = "Less then 0";
+    console.log(`2: Determiner is "${updater}"`);
+  }
+}
+
+// console.log(message) would not print outside of the if case, since the var is declared inside and therefore has the scope of the block after if
+
+// 3.
+
+/* I personally think at the moment, that I would only use it for really simple true/ false cases within string interpolation. In general it is usable whenever it is eual to a simple `if{} else{}` case */
+
+/*
+16-PB-conditionals-pt2/16-daniel.js:30
+console.log(`4: Is Mark's BMI higher then John's: It is ${!isBmiHigher ? "not" : ""}.`);
+*/

d167bb712edef1cb53d60463bcc5dda06388a726
dk1code <dk1code@web.de>
2020-06-01 14:39:26 +0200

    overcomplicating-19.js -v 0.2

--- 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
+++ 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
@@ -1,8 +1,27 @@
 "use strict";
 
-let jsonObj = { name: "PB Exercise 19", version: "0.1", addinfo: "This ist to tryout JSON with Sting Interpolation" };
+let jsonObj = { name: "PB Exercise 19", version: "0.2", addinfo: "This ist to tryout while-loop" };
 
+console.log("**********************************************************************************\n");
 console.log(
-    `Welcome to ${jsonObj.name}. This is version: ${jsonObj.version}.`)
-console.log(`Additional Information was given to this run: ${jsonObj.addinfo ? `\n${jsonObj.addinfo}` : false}`
-)
+  `Welcome to\x1b[31m ${jsonObj.name}\x1b[0m. This is version:\x1b[31m ${jsonObj.version}\x1b[0m.`
+);
+console.log(
+  `Additional Information was given to this run: \x1b[31m \n"${
+    jsonObj.addinfo ? `${jsonObj.addinfo}` : false
+  }\x1b[0m"`
+);
+console.log(
+  "\n**********************************************************************************\n"
+);
+
+let isRunning = true, i =0;
+while (isRunning === true) {
+    
+    if (i < 10 === true) {
+        console.log(`This is run: ${i + 1}`);
+    } else {
+        isRunning = false;
+    }
+    i++;
+}

fc016ef767f639e72bff377b08018bb7477965b6
dk1code <dk1code@web.de>
2020-06-01 18:46:02 +0200

    overcomplicating-19.js -v 0.3.3

--- 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
+++ 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
@@ -1,8 +1,15 @@
 "use strict";
 
-let jsonObj = { name: "PB Exercise 19", version: "0.3.2", addinfo: "Establishing an interface: 1st try with addListener() --> Not the required result. Works all the time, only" };
+let jsonObj = {
+  name: "PB Exercise 19",
+  version: "0.3.3",
+  addinfo:
+    "Establishing an interface: 3rd try with inquirer --> Work not as required. Learning is, that a loop might not the right approach.",
+};
 
-console.log("**********************************************************************************\n");
+console.log(
+  "**********************************************************************************\n"
+);
 console.log(
   `Welcome to\x1b[31m ${jsonObj.name}\x1b[0m. This is version:\x1b[31m ${jsonObj.version}\x1b[0m.`
 );
@@ -15,12 +22,25 @@ console.log(
   "\n**********************************************************************************\n"
 );
 
-let stdin = process.openStdin();
 
-let isRunning = true, inputVar = null;
+
+let isRunning = true,
+  inputVar = null;
 while (isRunning === true) {
-    
-  stdin.addListener("data", function (d) { inputVar = d });
-    console.log(`\n\nThis was your input:${inputVar}`);
-    isRunning = false;
+
+  const inquirer = require("inquirer");
+
+  let question = [
+    {
+      type: "input",
+      name: "input",
+      message: "Please type a number!",
+    },
+  ];
+  inquirer.prompt(question).then((answers) => {
+    inputVar = `${answers('name')}`
+  });
+
+  console.log(`\n\nThis was your input:${inputVar}`);
+  isRunning = false;
 }

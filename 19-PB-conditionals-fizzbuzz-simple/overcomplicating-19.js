e25c82bf8dfb0b1410b51dc8115c4a59f8a8cb43
dk1code <dk1code@web.de>
2020-06-02 09:21:43 +0200

    overcomplicating-19.js -v 0.4.1

--- 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
+++ 19-PB-conditionals-fizzbuzz-simple/overcomplicating-19.js
@@ -2,9 +2,9 @@
 
 let jsonObj = {
   name: "PB Exercise 19",
-  version: "0.3.3",
+  version: "0.4.1",
   addinfo:
-    "Establishing an interface: 3rd try with inquirer --> Work not as required. Learning is, that a loop might not the right approach.",
+    "Have established an interface with readline. But it doesn't work for all cases. But for the task values it gives the right results",
 };
 
 console.log(
@@ -22,25 +22,56 @@ console.log(
   "\n**********************************************************************************\n"
 );
 
+// taken from https://stackoverflow.com/questions/24182097/node-js-endless-loop-function-quit-upon-certain-user-input
+const readline = require("readline");
 
+function replDemo() {
+  return new Promise(function (resolve, reject) {
+    let rl = readline.createInterface(process.stdin, process.stdout);
+    rl.setPrompt("Give an integer> ");
+    rl.prompt();
+    rl.on("line", function (line) {
+      if (line === "q" || line === "quit" || line === "exit") {
+        rl.close();
+        return;
+      }
 
-let isRunning = true,
-  inputVar = null;
-while (isRunning === true) {
-
-  const inquirer = require("inquirer");
-
-  let question = [
-    {
-      type: "input",
-      name: "input",
-      message: "Please type a number!",
-    },
-  ];
-  inquirer.prompt(question).then((answers) => {
-    inputVar = `${answers('name')}`
+      // if (typeof(line) === "number") {
+        let inputVar = parseInt(line);
+      console.log(inputVar, typeof (inputVar));
+      if (inputVar == "NaN") {
+        console.log("This was not an integer")
+      }
+        else if (inputVar % 3 === 0 && inputVar % 5 === 0) {
+          console.log("FizzBuzz");
+        } else if (inputVar % 3 === 0) {
+          console.log("Fizz");
+        } else if (inputVar % 5 === 0) {
+          console.log("Buzz");
+        } else {
+          console.log("Try again!")
+        }
+        // rl.prompt();
+      // } else {
+      //   console.log("\x1b[5m\x1b[32mThis was not an integer\x1b[0m");
+        rl.prompt();
+      
+    
+      rl.prompt();
+    }).on("close", function () {
+      console.log("\nRun was endet");
+      // resolve(42); // this is the final result of the function
+    });
   });
-
-  console.log(`\n\nThis was your input:${inputVar}`);
-  isRunning = false;
 }
+
+async function run() {
+  try {
+    let replResult = await replDemo();
+    console.log("repl result:", replResult);
+  } catch (e) {
+    console.log("failed:", e);
+  }
+}
+
+run();

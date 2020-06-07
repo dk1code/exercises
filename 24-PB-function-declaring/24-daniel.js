635ff8f510f0cb1eaeacbcca6323853b663a57b4
dk1code <dk1code@web.de>
2020-06-07 17:20:55 +0200

    finished 24

---
+++ 24-PB-function-declaring/24-daniel.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// 1 *** Create a function that multiples a number by another number.
+
+function myFunction(x = 0, y = 1) {
+  console.log(x * y);
+}
+
+// 2 *** Rework the syntax of the above function so that the function declaration is stored as a value.
+
+const MY_FUNCTION = function (x = 0, y = 1) {
+  console.log(x * y);
+};
+
+// 3 *** Rework the syntax of the function declaration so that is uses the arrow function shorthand.
+
+const MULIPL_FUNC = (x = 0, y = 1) => {
+  console.log(x * y);
+};
+
+// 4. *** reate functions (using all three declarations) to check the remainder of division given two numbers.
+
+const DIV_FUNC = (x = 0, y = 1) => {
+  console.log(x / y);
+};

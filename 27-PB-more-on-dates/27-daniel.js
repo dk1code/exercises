f68d2e7b8394872d489222eaf5166ce5fa00cb01
dk1code <dk1code@web.de>
2020-06-08 15:14:19 +0200

    finished 27

---
+++ 27-PB-more-on-dates/27-daniel.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let inputDate = new Date("2020-09-14");
+
+const daysRemaining = (date) => {
+  let input = date.getDate();
+  let compare = null;
+
+  switch (date.getMonth()) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+    case 7:
+    case 9:
+    case 11:
+      compare = 31;
+      break;
+    case 3:
+    case 5:
+    case 8:
+    case 10:
+      compare = 30;
+      break;
+    case 1:
+      compare = 28;
+      break;
+    default:
+      console.log("***SWITCH ERROR***");
+  }
+
+  if (compare - input > 1) {
+    console.log(`There are ${compare - input} days to the end of the month.`);
+  } else if (compare - input === 1) {
+    console.log("There is one day to the end of the month.");
+  } else if (compare - input === 0) {
+      console.log("This is the last day of the month.");
+  } else {
+      console.log("***IF BLOCK ERROR***");
+  }
+};
+
+daysRemaining(inputDate);

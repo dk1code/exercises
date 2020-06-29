871f0109998d03d822cba6c8d0ace70b2adee8b8
dk1code <dk1code@web.de>
2020-06-29 15:41:40 +0200

    finished 42

---
+++ 42-PB-datastructure-destructuring/42.daniel.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// 1.
+
+console.log("1:");
+
+let [fruit, vegtable, ...food] = ["banana", "cucumber", "bread", "cakes", "pizza"];
+
+console.log(fruit + "\n" + vegtable + "\n" + food);
+
+// 2.
+
+console.log("\n2:");
+
+const costumes = {
+    fran: "witch",
+    elsa: "princess"
+};
+
+console.log(costumes);
+
+let {
+    fran,
+    elsa
+} = costumes;
+
+console.log(fran + "\n" + elsa);
+
+// 3.
+
+console.log("\n3:");
+
+const bands = {
+    name: "Regina Spektor",
+    artist: "singer/ songwriter and pianist",
+    nationality: "American-Russian",
+    genre: "indie-pop",
+    greatestHit: "Us"
+};
+
+// unnecessary sh*t on the side
+    
+const startsWithVowel = string => {
+    let firstChar = string[0].toLowerCase();
+    return (firstChar === "a" || firstChar === "e" || firstChar === "i" || firstChar === "o" || firstChar === "u")
+}
+
+const bandsFunc = ({
+    name,
+    artist,
+    nationality,
+    genre,
+    greatestHit
+}) =>
+    `${name} is a${startsWithVowel(nationality) ? "n" : ""} ${nationality} ${artist}. The musician sings ${genre} and their greatest hit is "${greatestHit}"`;
+
+console.log(bandsFunc(bands));

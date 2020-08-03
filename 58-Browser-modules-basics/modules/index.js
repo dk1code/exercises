35eb8cdaef46e8cd334d963fde1787466f251051
dk1code <dk1code@web.de>
2020-08-03 23:31:18 +0200

    58 done

---
+++ 58-Browser-modules-basics/modules/index.js
@@ -0,0 +1,15 @@
+"use strict"
+
+import {
+    clickImages
+} from "./click.js"
+import {
+    hoverHeader
+} from "./hover.js"
+import {
+    scrollWindow
+} from "./scroll.js"
+
+clickImages();
+hoverHeader();
+scrollWindow();
